357b5b7646ee27a81ce475533bb078b739761f65
Lucian Parfeni <lucian.parfeni@cynny.com>
2015-08-02 15:12:55 +0300

    replaced "

--- script/script.js
+++ script/script.js
@@ -35,19 +35,19 @@ todo:
 
 var colors = {
 	// sidebar | lines | squares | L | I | -L | T | square | z | -z | brick
-	0: ["#59b", "#59b", "#379", "#ff0", "#f0f", "#0ff", "#f00", "#0f0", "#00f", "#f80", "#fff"], // classic
-	1: ["#28284a", "#28284a", "#224", "#C794FF", "#871EB7", "#FFA7B4", "#6A14C8", "#FF0072", "#8EB5F1", "#40BCCB",
-		"#fff"
+	0: ['#59b', '#59b', '#379', '#ff0', '#f0f', '#0ff', '#f00', '#0f0', '#00f', '#f80', '#fff'], // classic
+	1: ['#28284a', '#28284a', '#224', '#C794FF', '#871EB7', '#FFA7B4', '#6A14C8', '#FF0072', '#8EB5F1', '#40BCCB',
+		'#fff'
 	], // lilac
-	2: ["#803008", "#803008", "#4d1d05", "#C23302", "#68910C", "#871C0A", "#CA2B1D", "#CF4935", "#DE9546",
-		"#F4B806", "#fff"
+	2: ['#803008', '#803008', '#4d1d05', '#C23302', '#68910C', '#871C0A', '#CA2B1D', '#CF4935', '#DE9546',
+		'#F4B806', '#fff'
 	], // bonfire
-	3: ["#008C9E", "#00DFFC", "#00B4CC", "#69D2E7", "#E0E4CC", "#A7DBD8", "#fa3600", "#68a8e8", "#F38630",
-		"#FA6900", "#fff"
+	3: ['#008C9E', '#00DFFC', '#00B4CC', '#69D2E7', '#E0E4CC', '#A7DBD8', '#fa3600', '#68a8e8', '#F38630',
+		'#FA6900', '#fff'
 	], // giant goldfish
-	4: ["#59b", "#59b", "#379", "#aa0", "#a0a", "#0aa", "#a00", "#0a0", "#00a", "#a80", "#eee"], // lego
-	5: ["#8b956d", "#c4cfa1", "#c4cfa1", "#6b7353", "#8b956d", "#6b7353", "#8b956d", "#6b7353", "#8b956d",
-		"#6b7353", "#414141"
+	4: ['#59b', '#59b', '#379', '#aa0', '#a0a', '#0aa', '#a00', '#0a0', '#00a', '#a80', '#eee'], // lego
+	5: ['#8b956d', '#c4cfa1', '#c4cfa1', '#6b7353', '#8b956d', '#6b7353', '#8b956d', '#6b7353', '#8b956d',
+		'#6b7353', '#414141'
 	], // lego
 };
 
@@ -249,8 +249,8 @@ var oneTimeIni = function() {
 
 	ctx.lineWidth = 2;
 
-	addEventListener("keydown", keydown);
-	addEventListener("keyup", keyup);
+	addEventListener('keydown', keydown);
+	addEventListener('keyup', keyup);
 
 	// create radial gradient
 	grd = ctx.createRadialGradient(150, 150, 100, 150, 150, 300);
@@ -405,17 +405,17 @@ var drawSidebar = function() {
 
 	//score
 	ctx.fillStyle = colors[theme][10];
-	ctx.font = "24px 'Press Start 2P'";
-	ctx.fillText("Next", sidebarX + 20, 30);
-	ctx.fillText("Score", sidebarX + 10, 360);
+	ctx.font = '24px \'Press Start 2P\'';
+	ctx.fillText('Next', sidebarX + 20, 30);
+	ctx.fillText('Score', sidebarX + 10, 360);
 	ctx.fillText(score, sidebarX + 20, 400);
-	ctx.fillText("Level", sidebarX + 10, 480);
+	ctx.fillText('Level', sidebarX + 10, 480);
 	ctx.fillText(level, sidebarX + 20, 520);
 
 	//pause
-	ctx.fillStyle = "#ff0";
+	ctx.fillStyle = '#ff0';
 	if (pause) {
-		ctx.fillText("Pause", sidebarX + 10, 300);
+		ctx.fillText('Pause', sidebarX + 10, 300);
 	}
 
 	setShadow();
@@ -471,8 +471,8 @@ var newBrick = function() {
 
 	draw();
 
-	if (noTouch("down") == 0) {
-		console.log("die");
+	if (noTouch('down') == 0) {
+		console.log('die');
 		pause = true;
 	}
 };
@@ -492,13 +492,13 @@ var clearLines = function() {
 		for (var i = 17; i >= 0; i--) {
 			for (var k = 0; k < check.length; k++) {
 				if (check[k] == i) {
-					//console.log("logg");
+					//console.log('logg');
 					line = true;
 					break;
 				}
 			}
 			if (line == false) {
-				//console.log("logxg");
+				//console.log('logxg');
 				for (j = i * 10; j < i * 10 + 10; j++) {
 					wall[j + (minus * 10)] = tempWall[j];
 					wallCol[j + (minus * 10)] = tempWallCol[j];
@@ -512,7 +512,7 @@ var clearLines = function() {
 };
 
 var checkLines = function() {
-	//console.log("log");
+	//console.log('log');
 
 	var ret = new Array();
 
@@ -557,7 +557,7 @@ var endBrick = function() {
 var advance = function() {
 	if (!pause) {
 		curBrick.y++;
-		if (noTouch("down") == 0) {
+		if (noTouch('down') == 0) {
 			curBrick.y--;
 			endBrick();
 		}
@@ -572,20 +572,20 @@ var noTouch = function(where) {
 
 	for (i = 0; i < 16; i++) {
 		if (shapes[curBrick.shape][curBrick.rotation * 16 + i]) {
-			//  console.log("not if");
+			//  console.log('not if');
 			if (curBrick.x + i % 4 > 9) {
-				//console.log("no toci right 2 ");
+				//console.log('no toci right 2 ');
 				right = 1;
 			} else if (
 				wall[(curBrick.y + Math.floor(i / 4)) * 10 + curBrick.x + i % 4] === 1) {
-				// console.log("no toci right ");
+				// console.log('no toci right ');
 				right = 1;
 			}
 
 			if (curBrick.x + i % 4 < 0)
 				left = 1;
 			else if (wall[(curBrick.y + Math.floor(i / 4)) * 10 + curBrick.x + i % 4] == 1) {
-				//console.log("no toci left");
+				//console.log('no toci left');
 				left = 1;
 			}
 
@@ -594,23 +594,23 @@ var noTouch = function(where) {
 		}
 	}
 
-	//console.log ("not " + where);
-	if (where == "right" && right == 1) {
-		//console.log("right blocked");
+	//console.log ('not ' + where);
+	if (where == 'right' && right == 1) {
+		//console.log('right blocked');
 		return 0;
 	}
-	if (where == "left" && left == 1) {
-		//console.log("left blocked");
+	if (where == 'left' && left == 1) {
+		//console.log('left blocked');
 		return 0;
 	}
-	if (where == "down" && down == 1) {
+	if (where == 'down' && down == 1) {
 		return 0;
 	}
-	if (where == "rotate" && (right == 1 || left == 1 || down == 1)) {
-		// console.log("rotate");
+	if (where == 'rotate' && (right == 1 || left == 1 || down == 1)) {
+		// console.log('rotate');
 		return 0;
 	}
-	//console.log ("ret 1");
+	//console.log ('ret 1');
 	return 1;
 
 	//return right + left + down;
@@ -618,8 +618,8 @@ var noTouch = function(where) {
 
 var moveRight = function() {
 	curBrick.x++;
-	//console.log("no toci = " + noTouch() );
-	if (noTouch("right") == 0) {
+	//console.log('no toci = ' + noTouch() );
+	if (noTouch('right') == 0) {
 		//console.log (curBrick.x);
 		curBrick.x--;
 	}
@@ -627,8 +627,8 @@ var moveRight = function() {
 
 var moveLeft = function() {
 	curBrick.x--;
-	//console.log("no toci 2 = " + noTouch() );
-	if (noTouch("left") == 0) {
+	//console.log('no toci 2 = ' + noTouch() );
+	if (noTouch('left') == 0) {
 		//console.log(noTouch());
 		curBrick.x++;
 	}
@@ -636,8 +636,8 @@ var moveLeft = function() {
 
 var moveDown = function(argument) {
 	curBrick.y++;
-	//console.log("no toci = 3 " + noTouch() );
-	if (noTouch("down") == 0) {
+	//console.log('no toci = 3 ' + noTouch() );
+	if (noTouch('down') == 0) {
 		curBrick.y--;
 	}
 };
@@ -666,8 +666,8 @@ var moveOne = function(where) {
 		switch (where) {
 			case 1:
 				curBrick.x++;
-				//console.log("no toci = " + noTouch() );
-				if (noTouch("right") == 0) {
+				//console.log('no toci = ' + noTouch() );
+				if (noTouch('right') == 0) {
 					//console.log (curBrick.x);
 					curBrick.x--;
 				}
@@ -675,8 +675,8 @@ var moveOne = function(where) {
 				break;
 			case -1:
 				curBrick.x--;
-				//console.log("no toci 2 = " + noTouch() );
-				if (noTouch("left") == 0) {
+				//console.log('no toci 2 = ' + noTouch() );
+				if (noTouch('left') == 0) {
 					//console.log(noTouch());
 					curBrick.x++;
 				}
@@ -684,8 +684,8 @@ var moveOne = function(where) {
 				break;
 			case 2:
 				curBrick.y++;
-				//console.log("no toci = 3 " + noTouch() );
-				if (noTouch("down") == 0) {
+				//console.log('no toci = 3 ' + noTouch() );
+				if (noTouch('down') == 0) {
 					curBrick.y--;
 				}
 				draw();
@@ -696,18 +696,18 @@ var moveOne = function(where) {
 
 var rotate = function() {
 	if (!pause) {
-		//console.log( "1 rotate: " + curBrick.rotation + " noTouch: " + noTouch() );
+		//console.log( '1 rotate: ' + curBrick.rotation + ' noTouch: ' + noTouch() );
 		if (curBrick.rotation < 3) {
-			//  console.log( "rotate: " + curBrick.rotation + " noTouch: " + noTouch() );
+			//  console.log( 'rotate: ' + curBrick.rotation + ' noTouch: ' + noTouch() );
 			curBrick.rotation++;
-			if (noTouch("rotate") == 0) {
-				if (noTouch("down") == 0) {
+			if (noTouch('rotate') == 0) {
+				if (noTouch('down') == 0) {
 					curBrick.rotation--;
 				} else {
-					while (noTouch("right") == 0 && noTouch("left") == 1) {
+					while (noTouch('right') == 0 && noTouch('left') == 1) {
 						curBrick.x--;
 					}
-					while (noTouch("right") == 1 && noTouch("left") == 0) {
+					while (noTouch('right') == 1 && noTouch('left') == 0) {
 						curBrick.x++;
 					}
 					//else
@@ -716,14 +716,14 @@ var rotate = function() {
 			}
 		} else if (curBrick.rotation == 3) {
 			curBrick.rotation = 0;
-			if (noTouch("rotate") == 0) {
-				if (noTouch("down") == 0) {
+			if (noTouch('rotate') == 0) {
+				if (noTouch('down') == 0) {
 					curBrick.rotation = 3;
 				} else {
-					while (noTouch("right") == 0 && noTouch("left") == 1) {
+					while (noTouch('right') == 0 && noTouch('left') == 1) {
 						curBrick.x--;
 					}
-					while (noTouch("right") == 1 && noTouch("left") == 0) {
+					while (noTouch('right') == 1 && noTouch('left') == 0) {
 						curBrick.x++;
 					}
 					//else
@@ -750,11 +750,6 @@ var keydown = function(e) {
 				moving = 1;
 			}
 		}, 200);
-
-
-
-		// moving = 1;
-
 	} else if (e.keyCode == 37) { //left
 		keyIsDown = true;
 
@@ -786,7 +781,7 @@ var keydown = function(e) {
 
 	}
 
-	console.log("key " + e.keyCode, keyIsDown, moving);
+	console.log('key ' + e.keyCode, keyIsDown, moving);
 
 
 	//else if ( e.keyCode == 32 )
@@ -795,7 +790,7 @@ var keydown = function(e) {
 
 var keyup = function(e) {
 	if ((e.keyCode == 39 && moving == 1) || (e.keyCode == 37 && moving == -1) || (e.keyCode == 40 && moving == 2)) {
-		//console.log("uppy");
+		//console.log('uppy');
 
 		keyIsDown = false;
 		moving = 0;
@@ -811,7 +806,7 @@ var keyup = function(e) {
 var pauseButton = document.getElementById('pause');
 pauseButton.addEventListener('click', function() {
 	pause = !pause;
-	alert("Alert!");
+	alert('Alert!');
 	draw();
 }, false);
 
@@ -826,14 +821,14 @@ themeButton.addEventListener('click', function() {
 		theme++;
 	else
 		theme = 0;
-	// document.getElementById('canvas').style.border = "8px solid " + colors[theme][10];
+	// document.getElementById('canvas').style.border = '8px solid ' + colors[theme][10];
 	draw();
 }, false);
 
 var levelButton = document.getElementById('level');
 levelButton.addEventListener('click', function() {
 	// ini();
-	// console.log('moar speed ' + speed[level] + " " + speed.length);
+	// console.log('moar speed ' + speed[level] + ' ' + speed.length);
 	pause = 1;
 	if (level + 1 >= speed.length)
 		level = -1;
